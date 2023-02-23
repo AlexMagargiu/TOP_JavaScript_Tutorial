e340733a43a1124bd73be18ea4e93bdc4a552fbb
AlexMagargiu <122966645+AlexMagargiu@users.noreply.github.com>
2023-02-23 10:55:20 +0200

    Complete the variable tutorial from javascript fundamentals part 1

---
+++ javascript.js
@@ -0,0 +1,47 @@
+let message = 'Hello!';
+alert(message);
+
+let user = 'John'
+let age = 25; 
+let messageTwo = 'Hello';
+
+let messageThree;
+messageThree = 'Hello!';
+messageThree = 'World!';
+alert(messageThree);
+
+let hello = 'Hello world!';
+let messageFour;
+messageFour = hello;
+alert(hello);
+alert(messageFour);
+
+let userName;
+let test123;
+
+let $ = 1;
+let _ = 2;
+alert($ + _);
+
+const myBirthday = '18.04.1982';
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+let color = COLOR_ORANGE;
+alert(color);
+
+// Task 1
+let admin;
+let name = "John";
+admin = name;
+alert(admin);
+
+//Task 2
+let ourPlanetName = "Earth";
+let currentUser = "John";
+
+//Task 3
+const BIRTHDAY = '18.04.1982';
+const age = someCode(birthday);
