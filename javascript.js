ce28b74a5d6e1891d90f70fa8ce55c56450d3b47
AlexMagargiu <122966645+AlexMagargiu@users.noreply.github.com>
2023-02-23 12:00:24 +0200

    Complete basic math article from MDN web docs

--- javascript.js
+++ javascript.js
@@ -1,47 +1,62 @@
-let message = 'Hello!';
-alert(message);
+//let message = 'Hello!';
+//alert(message);
 
-let user = 'John'
-let age = 25; 
-let messageTwo = 'Hello';
+//let user = 'John'
+//let age = 25; 
+//let messageTwo = 'Hello';
 
-let messageThree;
-messageThree = 'Hello!';
-messageThree = 'World!';
-alert(messageThree);
+//let messageThree;
+//messageThree = 'Hello!';
+//messageThree = 'World!';
+//alert(messageThree);
 
-let hello = 'Hello world!';
-let messageFour;
-messageFour = hello;
-alert(hello);
-alert(messageFour);
+//let hello = 'Hello world!';
+//let messageFour;
+//messageFour = hello;
+//alert(hello);
+//alert(messageFour);
 
-let userName;
-let test123;
+//et userName;
+//let test123;
 
-let $ = 1;
-let _ = 2;
-alert($ + _);
+//let $ = 1;
+//let _ = 2;
+//alert($ + _);
 
-const myBirthday = '18.04.1982';
+//const myBirthday = '18.04.1982';
 
-const COLOR_RED = "#F00";
-const COLOR_GREEN = "0F0";
-const COLOR_BLUE = "#00F";
-const COLOR_ORANGE = "#FF7F00";
-let color = COLOR_ORANGE;
-alert(color);
+//const COLOR_RED = "#F00";
+//const COLOR_GREEN = "0F0";
+//const COLOR_BLUE = "#00F";
+//const COLOR_ORANGE = "#FF7F00";
+//let color = COLOR_ORANGE;
+//alert(color);
 
 // Task 1
-let admin;
-let name = "John";
-admin = name;
-alert(admin);
+//let admin;
+//let name = "John";
+//admin = name;
+//alert(admin);
 
 //Task 2
-let ourPlanetName = "Earth";
-let currentUser = "John";
+//let ourPlanetName = "Earth";
+//let currentUser = "John";
 
 //Task 3
-const BIRTHDAY = '18.04.1982';
-const age = someCode(birthday);
+//const BIRTHDAY = '18.04.1982';
+//const age = someCode(birthday);
+
+const myInt = 5;
+const myFloat = 6.667;
+
+const lotsOfDecimal = 1.766584958675746364;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+
+let myNumber = "74";
+myNumber = Number(myNumber) + 3;
+
+const num1 = 10;
+const num2 = 50;
+
+let num3 = 4
+num3++;
